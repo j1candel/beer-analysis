145ca3cfe4709ddc6bf9eab9cd8168b7d79ef5dd
Jovany Candelario <jovany.candel@gmail.com>
2020-11-12 15:08:31 -0800

    added notebook

--- schema_code.sql
+++ schema_code.sql
@@ -1,4 +1,4 @@
--- Create Two Tables for CSV's
+/*CREATE TABLE beer_reviews*/
 CREATE TABLE beer_reviews (
 	ID SERIAL PRIMARY KEY,
 	brewery_id INT,
@@ -15,10 +15,8 @@ CREATE TABLE beer_reviews (
 	beer_abv DECIMAL,
 	beer_beerid INT
 );
-	
-SELECT * FROM beer_reviews
-DROP TABLE beer_reviews
 
+/*CREATE TABLE beer_reviews*/	
 CREATE TABLE breweries (
 	id INT PRIMARY KEY,
 	name VARCHAR,
@@ -34,34 +32,84 @@ CREATE TABLE breweries (
 	descript VARCHAR,
 	last_mod DATE
 );
+	
+/*JOINING TWO TABLES TOGETHER AND PICKING THE FIELDS WE WANT*/
+CREATE TABLE reviewed_breweries AS
+SELECT A.id,
+A.city,
+A.state,
+B.brewery_name,
+B.review_overall,
+B.review_aroma,
+B.review_appearance,
+B.beer_style,
+B.review_taste,
+B.beer_name,
+B.beer_abv FROM breweries A
+LEFT JOIN beer_reviews B
+ON A.name = B.brewery_name
 
-SELECT * FROM breweries 
-DROP TABLE breweries 
-	
-	
-	CREATE TABLE cleaned_breweries AS
-	SELECT B.id,
-	A.city,
-	A.state,
-	B.brewery_name,
-	B.review_overall,
-	B.review_aroma,
-	B.review_appearance,
-	B.beer_style,
-	B.review_taste,
-	B.beer_name,
-	B.beer_abv FROM breweries A
-	LEFT JOIN beer_reviews B
-	ON A.name = B.brewery_name
-	
-	B.id,
-	B.city,
-	B.state,
-	A.brewery_name,
-	A.review_overall,
-	A.review_aroma,
-	A.review_appearance,
-	A.beery_style,
-	A.review_taste,
-	A.beer_name,
-	A.beer_abv
+/*DELETED ROWS THAT DO NOT HAVE ACTUAL DATA*/
+delete from reviewed_breweries 
+where brewery_name is null
+
+/*CREATE NEW TABLE WITH NEW review_overall FIELD*/
+CREATE TABLE reviewed_breweires_clean AS
+select id 
+,city 
+,state
+,beer_style 
+,brewery_name 
+,review_overall 
+,beer_abv 
+,review_aroma
+,review_appearance
+,review_taste
+,CAST((SUM(review_overall)+SUM(review_aroma)+SUM(review_appearance)+SUM(review_taste))/4 AS DECIMAL(10,2))AS average_score
+from reviewed_breweries 
+GROUP BY id
+,city
+,state
+,brewery_name
+,review_overall
+,review_aroma
+,review_appearance
+,review_taste
+,beer_style
+,beer_abv
+
+/*ADD A FOREIGN KEY TO id*/
+ALTER TABLE reviewed_breweries ADD
+CONSTRAINT fk_id FOREIGN KEY(id) REFERENCES breweries(id)
+
+
+/*DROP UNWANTED FIELDS AND FILL IN NULL FIELDS FOR TABLE breweries*/
+CREATE TABLE cleaned_breweries AS 
+SELECT id
+,name
+,address1 as address
+,city
+,state
+,CASE WHEN code is null THEN 'no code' ELSE code END AS code
+,country
+,CASE WHEN phone is null THEN 'no phone' ELSE phone END AS phone
+,CASE WHEN website is null THEN 'no website' ELSE website END AS website
+,CASE WHEN descript is null THEN 'no description' ELSE descript END AS description
+FROM breweries;
+
+/*REFORMATING id AS A PRIMARY KEY*/
+ALTER TABLE cleaned_breweries ADD PRIMARY KEY (id)
+
+/*CREATING TABLE categories*/
+CREATE TABLE categories
+(id INT PRIMARY KEY,
+cat_name VARCHAR);
+
+/*CREATING TABLE breweires_geocode*/
+CREATE TABLE breweries_geocode (
+id SERIAL PRIMARY KEY,
+brewery_id INT,
+CONSTRAINT fk_brewery_id FOREIGN KEY(brewery_id) REFERENCES cleaned_breweries(id),
+latitude FLOAT,
+longitude FLOAT,
+accuracy VARCHAR)
